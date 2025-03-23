5b1329d348d06d0b5f15611a320e7971de627aa0
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2025-03-23 13:47:26 +0900

    feat: initial commit

---
+++ src/main/java/net/alcaris/plugin/core/AlcarisCore.java
@@ -0,0 +1,17 @@
+package net.alcaris.plugin.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AlcarisCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
